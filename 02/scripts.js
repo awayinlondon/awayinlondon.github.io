bb2ccaefbf5badabff2a49b63e6915d35e46a64c
Damien Barrett <damienbarrett@Damiens-MBP.lan>
2020-07-27 10:12:22 +1200

    Added javascript.

---
+++ 02/scripts.js
@@ -0,0 +1,6 @@
+const fileSelector = document.getElementById('file-selector');
+fileSelector.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    console.log(fileList);
+    alert(fileList)
+});
