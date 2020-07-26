9431cc9a70da8115aa05ec05f26aa820f78b6b12
Damien Barrett <damienbarrett@Damiens-MBP.lan>
2020-07-27 10:25:00 +1200

    Javascript to update html element text.

--- 02/scripts.js
+++ 02/scripts.js
@@ -1,7 +1,15 @@
+function updateText(newText) {
+    var textboxElement = document.getElementById('textbox');
+    textboxElement.innerHTML = newText;
+}
+
+
 const fileSelector = document.getElementById('file-selector');
 fileSelector.addEventListener('change', (event) => {
     const fileList = event.target.files;
     console.log(fileList);
-    alert(fileList)
+    sFilename = fileList[0].name;
+    updateText(sFilename)
     alert(fileList[0].name)
 });
+
