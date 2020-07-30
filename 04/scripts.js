af51bd34db8cc8b9ccea36afc2f822c6fbc617d4
awayinlondon <44626465+awayinlondon@users.noreply.github.com>
2020-07-30 14:08:51 +1200

    Update scripts.js

--- 04/scripts.js
+++ 04/scripts.js
@@ -15,7 +15,7 @@ function zipStuff(blob) {
         reader.getEntries(function(entries) {
         if (entries.length) {
             console.log(entries);
-            entries.map(zipStuff);
+            entries.map(listWrap);
             console.log("zipStuff: if entries.length");
             // get first entry content as text
             entries[0].getData(new zip.TextWriter(), function(text) {
